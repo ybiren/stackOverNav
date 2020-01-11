717c74ec839c32f9f0a4e86aae5331d909b889be
yossi1316 <bengold789@gmail.com>
2020-01-11 18:57:29 +0200

    commit 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {ViewerComponent} from './viewer/viewer.component';
 
 @NgModule({
   declarations: [
+    ViewerComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
